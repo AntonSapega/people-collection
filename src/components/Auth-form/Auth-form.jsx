5f8750578260d4124eb40b6017d511f6dc9881b0
Anton Sapeha <antonsapega@gmail.com>
2021-12-13 21:37:50 +0300

    There was added validation to form (using Formik) | Did refactoring

---
+++ src/components/Auth-form/Auth-form.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import styles from './Auth-form.module.scss';
+import { Formik } from 'formik';
+import StringButton from '../String-button/String-button';
+import PrimaryButton from '../Primary-button/Primary-button';
+
+export default class AuthForm extends React.Component {
+
+  constructor() {
+    super();
+
+    this.validators = this.validators.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    this.handleForgotPassword = this.handleForgotPassword.bind(this);
+  }
+
+  validators(values) {
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password.length < 3) {
+      errors.password = 'Invalid password (It should be more longer)'
+    }
+
+    return errors;
+  }
+
+  submitForm(submitEvent) {
+    console.log(submitEvent)
+  }
+
+  handleForgotPassword() {
+    alert('It happens');
+  }
+
+  render() {
+
+    const authForm = ({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+    }) => {
+      return (
+        <form className={styles['auth-form']} onSubmit={handleSubmit}>
+          <h2 className={styles['auth-form__title']}>Log in</h2>
+  
+          <label className={styles['auth-form__label']}>
+            Email
+            <input
+              className={styles['auth-form__input']}
+              type="email"
+              name="email"
+              placeholder='joe@email.com'
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+            />
+            <small className={styles['auth-form__warning-message']}>{ errors.email && touched.email && errors.email }</small>
+          </label>
+  
+          <label className={styles['auth-form__label']} htmlFor='password'>Password
+            <input
+              className={styles['auth-form__input']}
+              type="password"
+              name="password"
+              placeholder='Enter your Password'
+              id="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+            />
+            <small className={styles['auth-form__warning-message']}>{ errors.password && touched.password && errors.password }</small>
+          </label>
+          
+          <div className={styles['auth-form__tip-btn']}>
+            <StringButton handleClick={this.handleForgotPassword}>forgot password?</StringButton>
+          </div>
+
+          <PrimaryButton type={'submit'} isDisable={errors.email || errors.password || values.email.length === 0 || values.password.length === 0}>
+            Login
+          </PrimaryButton>
+        </form>
+      )
+    }
+
+    return (
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validate={this.validators}
+        onSubmit={this.submitForm}
+      >
+        {authForm}
+      </Formik>
+    )
+  }
+}
--- src/components/Login-form/Login-form.jsx
+++ src/components/Login-form/Login-form.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Login-form.module.scss';
+import AuthForm from '../Auth-form/Auth-form';
+import StringButton from '../String-button/String-button';
 
 export default class LoginForm extends React.Component {
 
@@ -7,39 +9,12 @@ export default class LoginForm extends React.Component {
     super();
 
     this.handleHelpCall = this.handleHelpCall.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleForgotPassword = this.handleForgotPassword.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-
-    this.state = {
-      email: '',
-      password: ''
-    }
   }
 
   handleHelpCall() {
     alert('You must log in');
   }
 
-  handleSubmit(event) {
-    console.log('Email: ', this.state.email);
-    console.log('Password: ', this.state.password);
-    event.preventDefault();
-  }
-
-  handleForgotPassword() {
-    console.log('Forgot password button was clicked');
-  }
-
-  handleInput(event) {
-    const typeOfInput = event.target.type;
-
-    this.setState(() => {
-      return {
-        [typeOfInput]: event.target.value
-      }
-    })
-  }
 
   render() {
     return (
@@ -50,33 +25,11 @@ export default class LoginForm extends React.Component {
           <span className={styles['login-form__description']}>{this.props.description}</span>
         </aside>
         <div className={styles['login-form__right-side']}>
-          <button className={styles['login-form__tip-btn']} onClick={this.handleHelpCall}>Need help?</button>
-          <form className={styles['login-form__form']} onSubmit={this.handleSubmit}>
-            <h2 className={styles['login-form__form-title']}>Log in</h2>
-
-            <label className={styles['login-form__form-label']} htmlFor='email'>Email</label>
-            <input
-              className={styles['login-form__form-input']}
-              type="email"
-              placeholder='joe@email.com'
-              id="email"
-              onInput={this.handleInput}
-              value={this.state.email}
-            />
-
-            <label className={styles['login-form__form-label']} htmlFor='password'>Password</label>
-            <input
-              className={styles['login-form__form-input']}
-              type="password"
-              placeholder='Enter your Password'
-              id="password"
-              onInput={this.handleInput}
-              value={this.state.password}
-            />
-            
-            <button className={styles['login-form__tip-btn']} type='button' onClick={this.handleForgotPassword}>forgot password?</button>
-            <button className={styles['login-form__btn']} type="submit">Login</button>
-          </form>
+          <div className={styles['login-form__btn-tip']}>
+            <StringButton handleClick={this.handleHelpCall}>Need help?</StringButton>
+          </div>
+          
+          <AuthForm />
         </div>
       </div>
     )
