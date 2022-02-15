ce404a3e7c72c214145afe589ceba41cca72c1e1
Anton Sapeha <antonsapega@gmail.com>
2022-02-15 20:07:09 +0300

    feature: add useScaleStyle hook

--- src/screens/Colors/ColorCard/ColorCard.jsx
+++ src/screens/Colors/ColorCard/ColorCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useRef } from 'react';
+import useScaleStyle from '../../../hooks/useScaleStyle';
 import styles from './ColorCard.module.scss';
 
+
+
 const ColorCard = ({onColorCard, color, name, style}) => {
+  const ref = useRef();
+  const isScaling = useScaleStyle(ref);
+
   return (
-    <figure style={{backgroundColor: color, ...style}} className={styles['color-card']} onClick={onColorCard}>
-      <figcaption className={styles['color-card__description']}>{name}</figcaption>
+    <figure ref={ref} style={{backgroundColor: color, ...isScaling, ...style}} className={styles['color-card']} onClick={onColorCard}>
+      <figcaption onClick={() => console.log(ref.current)} className={styles['color-card__description']}>{name}</figcaption>
     </figure>
   )
 }
